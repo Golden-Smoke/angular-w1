549d1946dff490b1bb0ecd88727d259a0b69aabe
Plamen <plamengeorgiev967@gmail.com>
2024-04-13 18:50:32 +0300

    Making Student Template

--- hello-world-2201681046/src/app/app.component.ts
+++ hello-world-2201681046/src/app/app.component.ts
@@ -1,6 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+type Student = {
+  name: string,
+  age: number,
+  hobbies: string[],
+  contentCreatorLink: string,
+  channel: Channel,
+  services: string[]
+}
+
+type Channel = {
+  channelTitle: string,
+  subs: number,
+  totalViews: number
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +24,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hello-world-2201681046';
+  title = 'Angular_Homework-1';
+  student: Student = {
+    name: "Plamen Georgiev",
+    age:20,
+    hobbies: [
+      "Mountain Hiking",
+      "Cooking",
+      "Calisthetics",
+      "Snowboarding"
+    ],
+    contentCreatorLink:"https://www.youtube.com/@programmingwithmosh",
+    channel: {
+      channelTitle:"Pako Rabani",
+      subs:1,
+      totalViews:10
+    },
+    services: [
+      "Editing photos with Adobe Photoshop",
+      "Editing videos with Adobe Premiere Pro",
+      "Making web applications"
+    ]
+  }
 }
